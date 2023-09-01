510b22eaa6982cee485e0b2e9a9d30c86a044f38
R41D3R0S <54575124+R41D3R0S@users.noreply.github.com>
2023-09-01 11:25:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $('.slider-area').owlCarousel(
+  {
+	  items: 1,
+	  loop: true,
+	  autoplay: true,
+	  nav: false,
+	  dots: true,
+	  animateOut: 'fadeOut'
+
+
+  });
+});
